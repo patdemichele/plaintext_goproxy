fcc3110af28828bc22131bf1ab775665d6f8aa8d
Patrick DeMichele <patd998@gmail.com>
2017-09-13 18:37:23 -0500

    trivially rejects everything\!

--- main.go
+++ main.go
@@ -3,18 +3,12 @@ import (
     "github.com/elazarl/goproxy"
     "log"
     "net/http"
-    "fmt"
 )
 
 func main() {
     proxy := goproxy.NewProxyHttpServer()
     proxy.Verbose = true
-    proxy.OnRequest().DoFunc(
-    func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
-    	fmt.Printf("Yeah I do this")
-    	r.Header.Set("X-GoProxy","yxorPoG-X")
-        return r,nil
-	})
+    proxy.OnRequest().HandleConnect(goproxy.AlwaysReject)
     log.Fatal(http.ListenAndServe(":8080", proxy))
 
 }
